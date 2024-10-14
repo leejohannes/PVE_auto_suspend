d4a9500989307ab20ef1344f6f83acf695648379
leejohannes <leeyoo@leeyoo.org>
2024-10-14 19:34:29 +0800

    Update uninstall_pve_auto_suspend.sh

--- uninstall_pve_auto_suspend.sh
+++ uninstall_pve_auto_suspend.sh
@@ -1,1 +1,8 @@
-
+echo "remove command reboot_new..."
+rm -f /usr/sbin/reboot_now rm -f /usr/sbin/reboot
+ln -s /usr/sbin/shutdown /usr/sbin/reboot
+echo "remove command poweroff_now..."
+rm -f /usr/sbin/poweroff_now /usr/sbin/poweroff
+ln -s /usr/sbin/shutdown /usr/sbin/poweroff
+echo "remove auto suspend script..."
+rm -f /var/lib/vz/snippets/pve-hibernate-all
